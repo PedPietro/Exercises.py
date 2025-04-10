f35e2c30f70cd89613830b9277a1c6481900e7ad
Pietro Tiago <166723178+PedPietro@users.noreply.github.com>
2025-04-10 09:42:47 -0300

    inicio cap 05

---
+++ cap05.py
@@ -0,0 +1,27 @@
+
+def ex5_2():
+    total = 2
+    for i in range(5) :
+        total = total * 2
+    print(f"O total é: {total}")
+
+#64
+
+# 5.3. Faça um programa em Python que imprima todos os múltiplos de N, entre um valor inicial e
+#um valor final.
+#Para saber se um número é múltiplo de N, você pode fazer if (numero % n == 0).
+#Os valores inteiros N, valor inicial e valor final devem ser lidos pelo teclado.
+def ex5_3():
+    valor_inicial = int(input("Digite o valor inicial: "))
+    valor_final = int(input("Digite o valor final: "))
+    N = int(input("Digite o valor de N: "))
+    if (valor_inicial % N == 0):
+        if(valor_final % N == 0):
+            total = 1
+            for x in range(valor_inicial,valor_final):
+                total = total * N
+                print(f"{x}")
+        else:
+            print("valor final inválido!")        
+    else:
+        print("valor inicial inválido!")
